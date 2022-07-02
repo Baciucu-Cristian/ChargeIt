573ff0f0fe0c747a282491ec7509ae8ff67c9680
Cristi <ridder-cristi@netrom.ro>
2022-07-02 10:22:33 +0300

    Session 2 and 3 tasks

---
+++ ChargeIt/Controllers/CarsController.cs
@@ -0,0 +1,61 @@
+﻿using ChargeIt.Data;
+using ChargeIt.Data.DbModels;
+using ChargeIt.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChargeIt.Controllers
+{
+	public class CarsController : Controller
+	{
+		private readonly ApplicationDbContext _applicationDbContext;
+
+		public CarsController(ApplicationDbContext applicationDbContext)
+		{
+			_applicationDbContext = applicationDbContext;
+		}
+
+		public IActionResult Index()
+		{
+			var carViewModels = _applicationDbContext.Cars.Select(c => new CarViewModel
+			{
+				Id = c.Id,
+				PlateNumber = c.PlateNumber 
+			}).ToList();
+
+			return View(carViewModels);
+		}
+
+		public IActionResult AddCar()
+		{
+			var carViewModel = new CarViewModel();
+
+			return View(carViewModel);
+		}
+
+		[HttpPost]
+		public IActionResult AddNewCar(CarViewModel carViewModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View("AddCar", carViewModel);
+			}
+
+			var existingCar = _applicationDbContext.Cars.FirstOrDefault(c => c.PlateNumber == carViewModel.PlateNumber);
+
+			if (existingCar != null)
+            {
+				ModelState.AddModelError("PlateNumber", "There is an already existing car with the same plate number");
+				return View("AddCar", carViewModel);
+			}
+
+			_applicationDbContext.Cars.Add(new CarDbModel
+			{
+				PlateNumber = carViewModel.PlateNumber,
+			});
+
+			_applicationDbContext.SaveChanges();
+
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ ChargeIt/Models/CarViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChargeIt.Models
+{
+	public class CarViewModel
+	{
+		public int Id { get; set; }
+        [Required]
+        [MaxLength(12)]
+        [Display(Name = "Plate Number")]		
+		public string PlateNumber { get; set; }
+	}
+}
