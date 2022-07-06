3d70810f6b7cd143ad7eac75c93faa06a023e363
Cristi <ridder-cristi@netrom.ro>
2022-07-06 09:53:15 +0300

    Added Bookings page

---
+++ ChargeIt/Controllers/BookingsController.cs
@@ -0,0 +1,49 @@
+﻿using ChargeIt.Data;
+using ChargeIt.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChargeIt.Controllers
+{
+    public class BookingsController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public BookingsController (ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var chargeMachineViewModels = _applicationDbContext.ChargeMachines.Select(cm => new ChargeMachineViewModel
+            {
+                Id = cm.Id,
+                City = cm.City,
+                Code = cm.Code,
+                Latitude = cm.Latitude,
+                Longitude = cm.Longitude,
+                Number = cm.Number,
+                Street = cm.Street
+            }).ToList();
+
+            var carViewModels = _applicationDbContext.Cars.Select(c => new CarViewModel
+            {
+                Id = c.Id,
+                PlateNumber = c.PlateNumber
+            }).ToList();
+
+            var bookingsViewModel = new BookingsViewModel()
+            {
+                ChargeMachines = chargeMachineViewModels,
+                Cars = carViewModels
+            };
+
+            return View(bookingsViewModel);
+        }
+
+        public IActionResult AddNewBooking()
+        {
+            return null;
+        }
+    }
+}
---
+++ ChargeIt/Models/BookingsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChargeIt.Models
+{
+    public class BookingsViewModel
+    {
+        public List<ChargeMachineViewModel> ChargeMachines { get; set; }
+        public List<CarViewModel> Cars { get; set; }
+        [Display(Name = "Charge Machine")]
+        public int ChargeMachineId { get; set; }
+        [Display(Name = "Car")]
+        public int CarId { get; set; }
+    }
+}
